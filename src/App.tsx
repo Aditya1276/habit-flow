3c471fdf6f21cf188625c397085e31101bb6888e
Aditya JAdhao <adityajadhav7902@gmail.com>
2026-03-03 15:09:05 +0530

    Fix router basename and SPA home link

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, "")}>
         <AppLayout>
           <Routes>
             <Route path="/" element={<DashboardPage />} />
